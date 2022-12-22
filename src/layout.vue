e917a9f416990315e724c9b8f9aef78c0cab007e
gaopan1213 <2920951613@qq.com>
2022-12-22 18:36:12 +0800

    first init

---
+++ src/layout.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-row class="wrap">
+    <el-col :span="3">
+        <div class="left">11111111111</div>
+    </el-col>
+    <el-col :span="21">
+        <div class="right">111111111111</div>
+    </el-col>
+  </el-row>
+</template>
+
+<style>
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.left {
+    background: red;
+    height: 100vh;
+}
+.right {
+    background: green;
+    height: 100vh;
+}
+.wrap {
+    width: 100%;
+}
+</style>
